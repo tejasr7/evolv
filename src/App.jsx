f09cafc182175abe4e579785e914894a1b376eed
Tejas Ranveer <39103436+tejasr7@users.noreply.github.com>
2024-11-15 05:41:54 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,8 @@ function App() {
   return (
     <>
       
-      <h1>Tejas</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>T</h1>
+      
     </>
   )
 }
