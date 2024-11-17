ecd4f221f01158e74e8b796c7a1863b10d44d841
Tejas Ranveer <39103436+tejasr7@users.noreply.github.com>
2024-11-17 15:19:30 +0530

    added about and readme

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,14 @@ function About() {
   return (
     <div>
       <h2>About Us</h2>
-      <p>This is the about page.</p>
+      <p>Our app is a unique blend of gaming and social networking, 
+        designed to bring people together in a fun and interactive way. 
+        Play engaging games, connect with friends, share moments, 
+        and even live stream your gameplay or experiences seamlessly. 
+        With a focus on community, entertainment, and security,
+         our platform offers a vibrant space to compete,
+          socialize, and create meaningful connections—all in one place.
+</p>
     </div>
   );
 }
